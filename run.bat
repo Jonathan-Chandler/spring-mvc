68b6bd9cfa75c89a96487a6a3bf6b7196b7eff8f
Jonathan Chandler <chandler.jonathan.g@gmail.com>
2022-08-28 18:06:17 -0700

    init

---
+++ run.bat
@@ -0,0 +1,10 @@
+call mvn spring-boot:run
+
+rem mvn clean tomcat7:redeploy
+rem call mvn clean package
+rem call mvn tomcat7:deploy
+rem mvn clean package -Dspring-boot.excludeDevtools=false -Dspring.devtools.restart.enabled=true
+
+rem call java -jar ./target/spring-boot-test-0.0.1-SNAPSHOT.jar
+rem call java -jar ./target/first-application-0.0.2-SNAPSHOT.war
+rem -Dspring.devtools.restart.enabled=true
