e681aa4c76814ddf2838807cad32f249f105a20d
Jonathan Chandler <chandler.jonathan.g@gmail.com>
2022-09-02 23:37:04 -0700

    react test

--- angular-main/src/app/app.component.spec.ts
+++ angular-main/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-test'`, () => {
+  it(`should have as title 'angular-main'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-test');
+    expect(app.title).toEqual('angular-main');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-test app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-main app is running!');
   });
 });
