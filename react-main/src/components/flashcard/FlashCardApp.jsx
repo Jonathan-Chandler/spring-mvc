15ba83ca07b95879a789b5981b9ac521ecfc78f7
Jonathan Chandler <chandler.jonathan.g@gmail.com>
2022-09-06 20:03:57 -0700

    unused import

--- react-main/src/components/flashcard/FlashCardApp.jsx
+++ react-main/src/components/flashcard/FlashCardApp.jsx
@@ -13,9 +13,6 @@ import LogoutComponent from './authentication/LogoutComponent.jsx'
 import ListTodosComponent from './pages/ListTodosComponent.jsx'
 import ErrorComponent from './pages/ErrorComponent.jsx'
 
-//import PropTypes from 'prop-types'
-//import {BrowserRouter as Router, Route, Routes, Link} from 'react-router-dom'
-//import AuthenticationService from './decorators/AuthenticationService.jsx'
 export class FlashCardApp extends Component 
 {
   render() 
