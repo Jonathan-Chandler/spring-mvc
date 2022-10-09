5098c70f6ea62d0913b45416cd24f76cadaa43eb
Jonathan Chandler <chandler.jonathan.g@gmail.com>
2022-10-09 01:32:44 -0700

    remove link

--- react-main/src/components/static/HeaderComponent.jsx
+++ react-main/src/components/static/HeaderComponent.jsx
@@ -10,7 +10,7 @@ export default function HeaderComponent(...params) {
         return (
             <header>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a href="http://127.0.0.1" className="navbar-brand">Something</a></div>
+                    <div><Link to="/" className="navbar-brand">Something</Link></div>
                     <ul className="navbar-nav">
                         {<li><NavLink className="nav-link" to="/welcome">Home</NavLink></li>}
                         {<li><NavLink className="nav-link" to="/todos">Todos</NavLink></li>}
@@ -25,7 +25,7 @@ export default function HeaderComponent(...params) {
         return (
             <header>
                 <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-                    <div><a href="http://127.0.0.1" className="navbar-brand">Something</a></div>
+                    <div><Link to="/" className="navbar-brand">Something</Link></div>
                     <ul className="navbar-nav">
                         {<li><NavLink className="nav-link" to="/welcome">Home</NavLink></li>}
                         {<li><NavLink className="nav-link" to="/login">Login</NavLink></li>}
